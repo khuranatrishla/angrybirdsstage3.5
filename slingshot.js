728e5099104fd11b8c2e24ccd2079555c464ce0b
trishla <trishlakhurana12@gmail.com>
2020-07-19 14:00:03 +0530

    slingshot

---
+++ slingshot.js
@@ -0,0 +1,28 @@
+class Slingshot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.pointB=pointB;
+        this.body = Constraint.create(options);
+        World.add(world, this.body);
+    }
+
+    fly(){
+        this.body.bodyA=null;
+    }
+  //null=false
+  //true
+    display(){
+        if(this.body.bodyA){
+            var pointA = this.body.bodyA.position;
+            var pointB = this.pointB;
+            strokeWeight(4);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+    }
+    
+}
